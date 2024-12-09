80be7958389f1894d0ec4faa02128929231fdb7d
BurylovBFC57 <burylovbfc@gmail.com>
2024-12-09 09:40:23 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let select = document.querySelector('select')
 let articles = document.querySelectorAll('article')
+let container = document.querySelector('.container')
 
 select.addEventListener('change', () => {
     for (const article of articles) {
@@ -10,3 +11,51 @@ select.addEventListener('change', () => {
         }
     }
 })
+
+let price = false
+
+function sortPrice(event){
+    price = !price
+    event.preventDefault();
+    container.innerHTML = ''
+    let selected = [...articles].sort((a,b) => {
+        return price ? a.dataset.price - b.dataset.price : b.dataset.price - a.dataset.price
+    })
+
+    for (const article of selected) {
+        container.appendChild(article)
+    }
+
+}
+
+let country = false
+
+function sortCountry(event){
+    country = !country
+    event.preventDefault();
+    container.innerHTML = ''
+    let selected = [...articles].sort((a,b) => {
+        return country ? a.dataset.country.localeCompare(b.dataset.country) : b.dataset.country.localeCompare(a.dataset.country)
+    })
+
+    for (const article of selected) {
+        container.appendChild(article)
+    }
+
+}
+
+let name1 = false
+
+function sortName(event){
+    name1 = !name1
+    event.preventDefault();
+    container.innerHTML = ''
+    let selected = [...articles].sort((a,b) => {
+        return name1 ? a.dataset.name.localeCompare(b.dataset.name) : b.dataset.name.localeCompare(a.dataset.name)
+    })
+
+    for (const article of selected) {
+        container.appendChild(article)
+    }
+
+}
